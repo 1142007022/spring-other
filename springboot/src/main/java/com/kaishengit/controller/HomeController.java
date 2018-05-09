5cf164e3d19f90cd2a818f3cee178e0fe8e0da7f
jiang <1142007022@qq.com>
2018-05-09 17:53:08 +0800

    测试springboot

--- springboot/src/main/java/com/kaishengit/controller/HomeController.java
+++ springboot/src/main/java/com/kaishengit/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.kaishengit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("message", "jiang");
 		return "home/home";
 	}
 	
