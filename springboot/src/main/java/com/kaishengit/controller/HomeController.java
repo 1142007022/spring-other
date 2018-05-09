1f6a33319a26f4bc5299582e8ce645754e2d0267
jiang <1142007022@qq.com>
2018-05-09 20:32:20 +0800

    测试thymeleaf

--- springboot/src/main/java/com/kaishengit/controller/HomeController.java
+++ springboot/src/main/java/com/kaishengit/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.kaishengit.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,12 @@ public class HomeController {
 	@GetMapping("/home")
 	public String home(Model model) {
 		model.addAttribute("message", "jiang");
+		model.addAttribute("age", 30);
+		List<String> lists = new ArrayList<>();
+		lists.add("1");
+		lists.add("2");
+		lists.add("3");
+		model.addAttribute("lists", lists);
 		return "home/home";
 	}
 	
