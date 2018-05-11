438e7fa569a50f580d0feb2f3c946de32a654181
jiang <1142007022@qq.com>
2018-05-11 22:52:55 +0800

    修复注解bug

--- SpringBoot-ActiveMQ/src/main/java/com/kaishengit/SpringActiveMqApplication.java
+++ SpringBoot-ActiveMQ/src/main/java/com/kaishengit/SpringActiveMqApplication.java
@@ -2,8 +2,10 @@ package com.kaishengit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class SpringActiveMqApplication {
 
 	public static void main(String[] args) {
