c90a4210fe1ab5d5a88cf269540a077384091007
jiang <1142007022@qq.com>
2018-06-05 16:41:06 +0800

    测试

---
+++ test/src/com/jiangdongit/Test.java
@@ -0,0 +1,14 @@
+package com.jiangdongit;
+
+public class Test {
+
+	public static void main(String[] args) {
+		for(int i = 0;i < 10;i++) {
+			if(i == 5) {
+				continue;
+			}
+			System.out.println("i----->" + i);
+		}
+	}
+
+}
